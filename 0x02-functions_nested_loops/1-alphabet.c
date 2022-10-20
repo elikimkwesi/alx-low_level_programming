e0e204dcaa25ec0f3becccb1ba44363ce78ff59d
elikimkwesi <elikimkwesi2003@gmail.com>
2022-10-20 01:52:14 -0700

    taskoand1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char lca;
+
+	while (lca = 'a'; lca <= 'z'; lca++)
+		_putchar(lca);
+
+	_putchar('\n');
+}
